6e6dac6f97e030fb891bc390e14114b2b4ef96ba
Anar <sadiglianar@gmail.com>
2021-05-22 20:45:36 +0400

    Shared module added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,41 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+const components = [];
+
+const modules = [
+  CommonModule,
+  RouterModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  LayoutModule,
+  MatSidenavModule,
+  MatListModule,
+  MatMenuModule,
+  MatCardModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule
+]
+
+
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...components, ...modules]
+})
+export class SharedModule { }
