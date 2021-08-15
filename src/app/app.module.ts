13af2490f6594ca1d926df38a6c4f20f19c22eed
Anar <sadiglianar@gmail.com>
2021-08-15 13:26:58 +0400

    routing added for the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { HomeComponent } from './home/home.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { LoginComponent } from './login/login.component';
 import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    KanbanComponent,
+    LoginComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
