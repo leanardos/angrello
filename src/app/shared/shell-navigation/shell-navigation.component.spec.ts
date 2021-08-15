c6179538afebd900527efe66f1fced9c966cd034
Anar <sadiglianar@gmail.com>
2021-08-15 20:36:15 +0400

    angular cli update

--- src/app/shared/shell-navigation/shell-navigation.component.spec.ts
+++ src/app/shared/shell-navigation/shell-navigation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ShellNavigationComponent } from './shell-navigation.component';
 
@@ -6,7 +6,7 @@ describe('ShellNavigationComponent', () => {
   let component: ShellNavigationComponent;
   let fixture: ComponentFixture<ShellNavigationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ShellNavigationComponent ]
     })
