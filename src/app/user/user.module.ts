8185a30039134339ee7630cb03a5c7e357cb0f7c
Anar <sadiglianar@gmail.com>
2021-08-19 00:05:34 +0400

    email login component created, work in progress

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './../shared/shared.module';
+import { EmailLoginComponent } from './email-login/email-login.component';
 import { GoogleSigninDirective } from './google-signin.directive';
 import { LoginComponent } from './login/login.component';
 import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
-  declarations: [GoogleSigninDirective, LoginComponent],
+  declarations: [GoogleSigninDirective, LoginComponent, EmailLoginComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
