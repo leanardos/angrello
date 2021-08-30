4bfa56bef50262625358f12988a0364efc8cd6d6
Anar <sadiglianar@gmail.com>
2021-08-30 22:53:08 +0400

    auth guard fix

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { SnackbarNotificationService } from './../services/snackbar-notification.service';
 
 @Injectable({
@@ -11,10 +10,10 @@ export class AuthGuard implements CanActivate {
 
   constructor(private angularFireAuth: AngularFireAuth, private snackbar: SnackbarNotificationService) {}
 
-  canActivate(
+  async canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const user = this.angularFireAuth.currentUser;
+    state: RouterStateSnapshot): Promise<boolean> {
+    const user = await this.angularFireAuth.currentUser;
     const isLoggedIn = !!user;
      
     if (!isLoggedIn) {
